9911720e629f19e21e4fe77aa04717e2c81d8d63
Muhammad Abdul Ghani Qureshi <itsaghani@gmail.com>
2024-08-25 10:54:13 +0500

    Add and substract functions are made

---
+++ vector.h
@@ -0,0 +1,41 @@
+//
+// Created by itsag on 8/25/2024.
+//
+
+#ifndef VECTOR_VECTOR_H
+#define VECTOR_VECTOR_H
+
+#include <utility>
+#include<vector>
+#include<iostream>
+
+namespace matrix {
+
+    class vector {
+    public:
+        static std::vector<int> add(std::vector<int> firstVector, std::vector<int> secondVector) {
+            std::vector<int> resultant(firstVector.size());
+            for (int i = 0; i < firstVector.size(); i++) {
+                resultant[i] = firstVector[i] + secondVector[i];
+            }
+            for (int i = 0; i < firstVector.size(); i++) {
+                std::cout << resultant[i] << " ";
+            }
+            return resultant;
+        }
+
+        static std::vector<int> substract(std::vector<int> firstVector, std::vector<int> secondVector) {
+            std::vector<int> resultant(firstVector.size());
+            for (int i = 0; i < firstVector.size(); i++) {
+                resultant[i] = firstVector[i] - secondVector[i];
+            }
+            for (int i = 0; i < firstVector.size(); i++) {
+                std::cout << resultant[i] << " ";
+            }
+            return resultant;
+        }
+    };
+
+} // matrix
+
+#endif //VECTOR_VECTOR_H
